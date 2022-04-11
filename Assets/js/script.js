7870b46d2f33943f1704f1d19d1adac782daf02c
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-04-11 09:29:18 -0400

    latest

---
+++ Assets/js/script.js
@@ -0,0 +1,23 @@
+//first thing is to wrapp document in a ready method
+$(document).ready(function () {
+  // Displaying current date and time
+
+  //creating new variable for current day
+  var rightNow = document.querySelector("#currentDay");
+  //special moment object that represents current time nd date
+  var currentTime = moment();
+  //seting the text content for current date and time
+  rightNow.textContent = currentTime.format("MMM DD, YYYY - hh:mm:ss a");
+
+  // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+//wait for button click to save values to local storage
+$(".saveBtn").on("click", function () {
+    var text = $(this);
+    console.log(text);
+  });
+
+});
+
+
+
