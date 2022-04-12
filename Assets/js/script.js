b2b857a33861f4434745c247925313fb533eecea
paulooliveira152012 <paulooliveira152012@gmail.com>
2022-04-12 10:28:30 -0400

    commit

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,28 +3,29 @@ $(document).ready(function () {
   // Displaying current date and time
 
   //creating new variable for current day
-  var rightNow = document.querySelector("#currentDay");
+  //   var rightNow = document.querySelector("#currentDay");
   //special moment object that represents current time nd date
-  var currentTime = moment();
+  //   var currentTime = moment();
   //seting the text content for current date and time
-  rightNow.textContent = currentTime.format("MMM DD, YYYY - hh:mm:ss a");
+  //   rightNow.textContent = currentTime.format("MMM DD, YYYY - hh:mm:ss a");
 
   // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-//Add event listener to parent container
-$(".container").on("click"), function() {
-    window.alert("Hey")
-}
 
+  //   var hour = document.getElementById();
+  //wait for button click to save values to local storage
+  $(".saveBtn").on("click", function () {
+    var value = $(this).siblings(".appointment").val();
+    var time = $(this).parent().attr("id");
 
-
-//wait for button click to save values to local storage
-// $(".saveBtn").on("click", function () {
-//     var text = $(this);
-//     console.log(text);
-//   });
-
+    //loacl storage takes two parameters => key and value
+    localStorage.setItem(time, value);
+  });
 });
 
+// bring data in from local storage to display on page
+
+$('#hour-8, .appointment').val(localStorage.getItem('hour-8'));
 
 
+// localStorage.getItem(key, value)
